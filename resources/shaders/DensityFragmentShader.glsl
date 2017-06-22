7a55073e5fb8ec1ede98303e38afdac214360c81
EmperorJack <emperorpurvis@gmail.com>
2017-06-22 20:32:24 +1200

    HSV to RGB for rainbow smoke

--- resources/shaders/DensityFragmentShader.glsl
+++ resources/shaders/DensityFragmentShader.glsl
@@ -9,6 +9,13 @@ uniform int screenHeight;
 
 uniform sampler2D densityTexture;
 
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main() {
     float density = texture(densityTexture, UV).r;
     float temperature = texture(densityTexture, UV).g;
@@ -16,6 +23,7 @@ void main() {
     float nx = gl_FragCoord.x / float(screenWidth);
     float ny = gl_FragCoord.y / float(screenHeight);
 
-    color = vec3(temperature, nx, ny) * (density * 5.0f);
+    color = hsv2rgb(vec3(nx, 1.0f, temperature)) * (density * 4.0f);
+//    color = vec3(temperature, nx, ny) * (density * 5.0f);
     //color = vec3(temperature, density, density);
 }
