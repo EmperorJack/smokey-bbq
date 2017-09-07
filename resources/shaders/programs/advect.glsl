780bec99c2af38be46f72c2a41aec74f3adca935
EmperorJack <emperorpurvis@gmail.com>
2017-09-07 18:34:16 +1200

    Reverted advect shader back to 2d fields only

--- resources/shaders/programs/advect.glsl
+++ resources/shaders/programs/advect.glsl
@@ -24,42 +24,42 @@ bool clampBoundary(inout float i) {
     return false;
 }
 
-vec3 getGridValue(sampler2D sampler, float i, float j) {
+vec2 getGridVelocity(sampler2D source, float i, float j) {
     bool boundary = !wrapBorders && (clampBoundary(i) || clampBoundary(j));
     vec2 texcoord = vec2(i, j) * inverseSize;
-    return texture(sampler, texcoord).xyz * (boundary ? 0.0f : 1.0f);
+    return texture(source, texcoord).xy * (boundary ? 0.0f : 1.0f);
 }
 
-vec2 getInterpolatedVelocity(float x, float y) {
+vec2 getInterpolatedVelocity(sampler2D source, float x, float y) {
 //    int i = (int(x + gridSize)) - gridSize;
 //    int j = (int(y + gridSize)) - gridSize;
     float i = x;
     float j = y;
 
-    return (i+1-x) * (j+1-y) * getGridValue(velocityTexture, i, j).xy +
-           (x-i) * (j+1-y)   * getGridValue(velocityTexture, i+1, j).xy +
-           (i+1-x) * (y-j)   * getGridValue(velocityTexture, i, j+1).xy +
-           (x-i) * (y-j)     * getGridValue(velocityTexture, i+1, j+1).xy;
+    return (i+1-x) * (j+1-y) * getGridVelocity(source, i, j) +
+           (x-i) * (j+1-y)   * getGridVelocity(source, i+1, j) +
+           (i+1-x) * (y-j)   * getGridVelocity(source, i, j+1) +
+           (x-i) * (y-j)     * getGridVelocity(source, i+1, j+1);
 }
 
-vec2 getVelocity(float x, float y) {
+vec2 getVelocity(sampler2D source, float x, float y) {
     float normX = x / float(gridSpacing);
     float normY = y / float(gridSpacing);
 
     vec2 v = vec2(0.0f, 0.0f);
 
     // Evaluating staggered grid velocities using central differences
-    v.x = (getInterpolatedVelocity(normX - 0.5f, normY).x +
-           getInterpolatedVelocity(normX + 0.5f, normY).x) * 0.5f;
-    v.y = (getInterpolatedVelocity(normX, normY - 0.5f).y +
-           getInterpolatedVelocity(normX, normY + 0.5f).y) * 0.5f;
+    v.x = (getInterpolatedVelocity(source, normX - 0.5f, normY).x +
+           getInterpolatedVelocity(source, normX + 0.5f, normY).x) * 0.5f;
+    v.y = (getInterpolatedVelocity(source, normX, normY - 0.5f).y +
+           getInterpolatedVelocity(source, normX, normY + 0.5f).y) * 0.5f;
 
     return v;
 }
 
 vec2 traceParticle(float x, float y) {
-    vec2 v = getVelocity(x, y);
-    v = getVelocity(x + (0.5f * timeStep * v.x), y + (0.5f * timeStep * v.y));
+    vec2 v = getVelocity(velocityTexture, x, y);
+    v = getVelocity(velocityTexture, x + (0.5f * timeStep * v.x), y + (0.5f * timeStep * v.y));
     return vec2(x, y) - (timeStep * v);
 }
 
@@ -67,6 +67,6 @@ void main() {
     vec2 pos = gl_FragCoord.xy;
 
     vec2 tracePosition = traceParticle(pos.x * gridSpacing, pos.y * gridSpacing);
-    vec3 newValue = getGridValue(sourceTexture, tracePosition.x / float(gridSpacing), tracePosition.y / float(gridSpacing));
-    color = vec4(newValue * dissipation, 0.0f);
+    vec2 newValue = getVelocity(sourceTexture, tracePosition.x, tracePosition.y);
+    color = vec4(newValue.xy * dissipation, 0.0f, 0.0f);
 }
